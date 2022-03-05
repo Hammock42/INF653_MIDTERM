445e21d3269a5ff4a865d3696e06142fc58ed269
Hamad Hamdi <Hammock42@protonmail.com>
2022-03-05 17:57:05 +0200

    Updated authors and categories api's

--- api/authors/read.php
+++ api/authors/read.php
@@ -1,3 +1,47 @@
 <?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
 
+    include_once '../../config/Database.php';
+    include_once '../../models/Author.php';
+
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate blog author object
+    $author = new Author($db);
+    
+    // author query
+    $result = $author->read();
+    // Get row count
+    $num = $result->rowCount();
+
+    // Check if any authors
+    if($num > 0) {
+        // author array
+        $authors_arr = array();
+        $authors_arr['data'] = array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $author_item = array(
+                'id' => $id,
+                'author' => $author
+            );
+
+            // Push to "data"
+            array_push($author_arr['data'], $author_item);
+        }
+
+        // Turn to JSON & output
+        echo json_encode($authors_arr);
+    } else {
+        // No authors
+        echo json_encode(
+            array('message' => 'No Authors Found')
+        );
+    }
 ?>
