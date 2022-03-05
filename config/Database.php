f61fa6541fbdacac43fcdd6f5dfac1249d51e6c5
Hamad Hamdi <Hammock42@protonmail.com>
2022-03-05 14:57:55 +0200

    First commit

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+    class Database {
+        // DB Params
+        private $host = '';
+        private $db_name = '';
+        private $username = '';
+        private $passwd = '';
+        private $conn;
+
+        // DB Connect
+        public function connect() {
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->passwd);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch(PDOException $e) {
+                echo 'Connection Error: ' . $e->getMessage();
+            }
+            return $this->conn;
+        }
+    }
+?>
---
+++ models/Quote.php
@@ -0,0 +1,118 @@
+<?php
+    class Quote {
+        // DB stuff
+        private $conn;
+        private $table = 'quotes';
+
+        // Quote Properties
+        public $id;
+        public $quote;
+        public $author;
+        public $author_id;
+        public $category;
+        public $category_id;
+
+        // Constructor with DB
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        // Get Quotes
+        public function read() {
+            // Create query
+            $query = 'SELECT
+                    q.id as id,
+                    q.quote as quote,
+                    a.author as author,
+                    c.category as category
+                FROM
+                    ' . $this->table . ' q
+                CROSS JOIN
+                    authors a ON a.id = q.authorId
+                CROSS JOIN
+                    categories c ON c.id = q.categoryId
+                ORDER BY
+                    q.id DESC';
+
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
+
+            // Execute Query
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        // Get Single Quote by quote id
+        public function read_single_q_id() {
+            // Create Query
+            $query = 'SELECT
+                    q.id as id,
+                    q.quote as quote,
+                    a.author as author,
+                    c.category as category
+                FROM
+                    ' . $this->table . ' q
+                CROSS JOIN
+                    authors a ON a.id = q.authorId
+                CROSS JOIN
+                    categories c ON c.id = q.categoryId
+                WHERE
+                    q.id = ?
+                LIMIT 0,1';
+
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind ID
+            $stmt->bindParam(1, $this->id);
+
+            // Execute Query
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // Set Properties
+            $this->id = $row['id'];
+            $this->quote = $row['quote'];
+            $this->author = $row['author'];
+            $this->category = $row['category'];
+        }
+
+        // Get Single Quote by author id
+        public function read_single_a_id() {
+            // Create Query
+            $query = 'SELECT
+                    q.id as id,
+                    q.quote as quote,
+                    a.author as author,
+                    c.category as category
+                FROM
+                    ' . $this->table . ' q
+                CROSS JOIN
+                    authors a ON a.id = q.authorId
+                CROSS JOIN
+                    categories c ON c.id = q.categoryId
+                WHERE
+                    q.authorId = ?
+                LIMIT 0,1';
+
+            // Prepare Statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind ID
+            $stmt->bindParam(1, $this->author_id);
+
+            // Execute Query
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // Set Properties
+            $this->id = $row['id'];
+            $this->quote = $row['quote'];
+            $this->author = $row['author'];
+            $this->category = $row['category'];
+        }
+    }
+?>
